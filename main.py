17fe82d4a6e40aec1852e558cd3f3713d8c27f7c
1arin <aroundthegreat@gmail.com>
2024-01-19 04:18:06 +0700

    add condition at def home

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ socketio = SocketIO(app)
 @app.route("/", methods=["POST", "GET"])
 
 def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
     # run html file called "home.html"
     return render_template("home.html")
 

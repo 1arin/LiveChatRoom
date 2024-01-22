20282858a9838a35ecffbb5f1a1945e7f010bb07
1arin <aroundthegreat@gmail.com>
2024-01-22 15:26:35 +0700

    add session , def room

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -22,6 +22,7 @@ def generate_unique_code(length):
 @app.route("/", methods=["POST", "GET"])
 
 def home():
+    session.clear()
     if request.method == "POST":
         name = request.form.get("name")
         code = request.form.get("code")
@@ -32,18 +33,25 @@ def home():
             return render_template("home.html", error="Please enter a name", code = code , name = name)
         
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code")
+            return render_template("home.html", error="Please enter a room code", code = code , name = name)
         
         room = code
         if create != False:
             room = generate_unique_code(4)
             room[room] = {"member: 0, messages": []}
         elif code not in room:
-            return render_template("home.html", error="Room does not exist.")
+            return render_template("home.html", error="Room does not exist.", code = code , name = name)
         
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
 
     # run html file called "home.html"
     return render_template("home.html")
+
+@app.route("/room")
+def rooom():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
